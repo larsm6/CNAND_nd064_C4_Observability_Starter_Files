6a318899c63061fe93c957bfc2c0956eed7a7cae
Ubuntu <maertins@changes.ngo>
2021-04-21 04:11:25 +0000

    added jaegger installation

---
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/scripts/conf/jaeger.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "**** Begin installation of Jaeger"
+export PATH=$PATH:/usr/local/bin
+
+#Create Namespace observability
+echo "**** Create Namespace observability"
+kubectl create namespace observability
+
+echo "**** Install CRD"
+kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/crds/jaegertracing.io_jaegers_crd.yaml
+
+echo "**** Install Service Account"
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/service_account.yaml
+
+# Configure Roles and Role Bindings
+echo "**** Configure Roles and Role Bindings"
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role.yaml
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role_binding.yaml
+
+echo "**** Install Jaeger Operator"
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/operator.yaml
+
+
+echo "**** End installation of Jaeger"
