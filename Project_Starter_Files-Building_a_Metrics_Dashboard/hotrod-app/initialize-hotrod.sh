0d0afd7ebff8aaf7b4fd2419e1a12cc30b698f6f
Ubuntu <maertins@changes.ngo>
2021-04-22 23:03:48 +0000

    Resolved an issue of the hotrod application initialization script

--- Project_Starter_Files-Building_a_Metrics_Dashboard/hotrod-app/initialize-hotrod.sh
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/hotrod-app/initialize-hotrod.sh
@@ -9,22 +9,35 @@ kubectl -n observability apply -f ./hotrod-app/hotrod-jaeger.yml
 
 # Get Deployments
 echo "***** Create Hotrod Service"
-kubectl -n observability apply -f ./hotrod-app/hotrod-service.yml
+kubectl apply -f ./hotrod-app/hotrod-service.yml
 
 # Deploy Hotrod Applicaiton
 echo "***** Deploy Hotrod Application"
-kubectl -n observability apply -f ./hotrod-app/hotrod-deployment.yml
+kubectl apply -f ./hotrod-app/hotrod-deployment.yml
 
 # Get Pods
 echo "***** Get Pods"
-kubectl -n observability get pods
+kubectl get pods
 
 # Get Services
 echo "***** Get Services"
-kubectl -n observability get services
+kubectl get services
 
 # Get Deployments
 echo "***** Get Deployments"
+kubectl get deployments
+
+
+# Get Pods
+echo "***** Get Pods from namespace observability"
+kubectl -n observability get pods
+
+# Get Services
+echo "***** Get Services from namespace observability"
+kubectl -n observability get services
+
+# Get Deployments
+echo "***** Get Deployments from namespace observability"
 kubectl -n observability get deployments
 
 echo "Forward Port of hotrod-query Load-Balancer"
